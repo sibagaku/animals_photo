ece41b0baa61565302fc7d36212b22fe54c69149
EC2 Default User <ec2-user@ip-172-31-6-5.ap-northeast-1.compute.internal>
2022-09-12 13:42:54 +0000

    update seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,19 @@ Admin.create!(
     email: "admin@admin",
     password: "admintest"
   )
+  
+ users = User.create!(
+    [
+      {email: "userA@test.com", password: "password", name: "テストユーザー1", self_introduction: "テストユーザー1人目", profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/test-user1.jpg"), filename: "test-user1.jpg")},
+      {email: "userB@test.com", password: "password", name: "テストユーザー2", self_introduction: "テストユーザー2人目", profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/test-user2.jpg"), filename: "test-user2.jpg")},
+      {email: "userC@test.com", password: "password", name: "テストユーザー3", self_introduction: "テストユーザー3人目", profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/test-user3.jpg"), filename: "test-user3.jpg")},
+    ] 
+  )
+  
+Post.create!(
+    [
+      {introduction: "街中で見つけた茶色い鳩", user_id: users[0].id, image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/test-post1.jpg"), filename: "test-post1.jpg")},
+      {introduction: "ぐっすり眠っているトラ", user_id: users[1].id, image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/test-post2.jpg"), filename: "test-post2.jpg")},
+      {introduction: "群れをなすヤギ", user_id: users[2].id, image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/test-post3.jpg"), filename: "test-post3.jpg")},
+    ]
+  )
