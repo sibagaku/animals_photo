d59d424402869ccd179dae85b6e587a43a2e6b27
EC2 Default User <ec2-user@ip-172-31-6-5.ap-northeast-1.compute.internal>
2022-09-13 13:45:07 +0000

    update db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,19 +10,58 @@ Admin.create!(
     email: "admin@admin",
     password: "admintest"
   )
-  
- users = User.create!(
+
+users = User.create!(
     [
       {email: "userA@test.com", password: "password", name: "テストユーザー1", self_introduction: "テストユーザー1人目", profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/test-user1.jpg"), filename: "test-user1.jpg")},
       {email: "userB@test.com", password: "password", name: "テストユーザー2", self_introduction: "テストユーザー2人目", profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/test-user2.jpg"), filename: "test-user2.jpg")},
       {email: "userC@test.com", password: "password", name: "テストユーザー3", self_introduction: "テストユーザー3人目", profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/test-user3.jpg"), filename: "test-user3.jpg")},
-    ] 
+    ]
   )
-  
-Post.create!(
+
+posts = Post.create!(
     [
       {introduction: "街中で見つけた茶色い鳩", user_id: users[0].id, image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/test-post1.jpg"), filename: "test-post1.jpg")},
       {introduction: "ぐっすり眠っているトラ", user_id: users[1].id, image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/test-post2.jpg"), filename: "test-post2.jpg")},
       {introduction: "群れをなすヤギ", user_id: users[2].id, image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/test-post3.jpg"), filename: "test-post3.jpg")},
     ]
   )
+
+comments = Comment.create!(
+    [
+      {post_id: posts[0].id, user_id: users[1].id, comment_text: "いいね"},
+      {post_id: posts[0].id, user_id: users[2].id, comment_text: "茶色い！"},
+      {post_id: posts[2].id, user_id: users[0].id, comment_text: "多い！"},
+    ]
+  )
+
+Favorite.create!(
+    [
+      {post_id: posts[0].id, user_id: users[1].id},
+      {post_id: posts[0].id, user_id: users[2].id},
+      {post_id: posts[2].id, user_id: users[0].id},
+    ]
+  )
+
+Follow.create!(
+    [
+      {followed_id: users[0].id, follower_id: users[1].id},
+      {followed_id: users[0].id, follower_id: users[2].id},
+      {followed_id: users[1].id, follower_id: users[0].id},
+    ]
+  )
+
+Notification.create!(
+    [
+      {visited_id: users[0].id, visitor_id: users[1].id, action: "follow"},
+      {visited_id: users[0].id, visitor_id: users[2].id, action: "follow"},
+      {visited_id: users[1].id, visitor_id: users[0].id, action: "follow"},
+      {visited_id: users[0].id, visitor_id: users[1].id, action: "favorite", post_id: posts[0].id},
+      {visited_id: users[0].id, visitor_id: users[2].id, action: "favorite", post_id: posts[0].id},
+      {visited_id: users[2].id, visitor_id: users[0].id, action: "favorite", post_id: posts[2].id},
+      {visited_id: users[0].id, visitor_id: users[1].id, action: "comment", post_id: posts[0].id, comment_id: comments[0].id },
+      {visited_id: users[0].id, visitor_id: users[2].id, action: "comment", post_id: posts[0].id, comment_id: comments[1].id },
+      {visited_id: users[2].id, visitor_id: users[0].id, action: "comment", post_id: posts[2].id, comment_id: comments[2].id },
+    ]
+  )
+
