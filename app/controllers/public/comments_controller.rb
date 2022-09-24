40e681ed9ec5cc57d69041b14b18c6be28852087
EC2 Default User <ec2-user@ip-172-31-6-5.ap-northeast-1.compute.internal>
2022-09-24 05:58:46 +0000

    updata comment-ajax

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -3,23 +3,24 @@ class Public::CommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    post = Post.find(params[:post_id])
-    comment = current_user.comments.new(comment_params)
-    comment.post_id = post.id
-    if comment.save
-      post.create_notification_comment!(current_user, comment.id)
-      redirect_to post_path(post)
+    @post = Post.find(params[:post_id])
+    @comment = current_user.comments.new(comment_params)
+    @comment.post_id = @post.id
+    if @comment.save
+      @post.create_notification_comment!(current_user, @comment.id)
+      render "comment"
     else
       @post = Post.find(params[:post_id])
-      @post_comment = comment
+      @post_comment = @comment
       @user = @post.user
       render template: "public/posts/show"
     end
   end
 
   def destroy
+    @post = Post.find(params[:post_id])
     Comment.find(params[:id]).destroy
-    redirect_to post_path(params[:post_id])
+    render "comment"
   end
 
   private
--- app/javascript/script.js
+++ app/javascript/script.js
@@ -12,3 +12,4 @@ $(function(){
     hideProvious: true,
   });
 });
+
